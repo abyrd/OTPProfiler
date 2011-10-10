c0165ec3b884113660246c08e9a113c0d54b2091
Andrew Byrd <andrew@fastmail.net>
2011-10-10 11:20:36 +0200

    accept and parse xml for now
    json response handles single-item and multi-item lists of itineraries differently
    see opentripplanner ticket 532

--- otpprofiler.py
+++ otpprofiler.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python
 
-import sqlite3
-db = sqlite3.connect("profile_results.sqlite")
+import sqlite3, urllib2, time, itertools
+# ubuntu package python-beautifulsoup
+from BeautifulSoup import BeautifulStoneSoup
+import random
 
-URL = "http://localhost/otp?fromPlace=%f,%f&toPlace=%f,%f&min=%s&maxWalkDistance=%d&mode=%s&submit&time=%s&arriveBy=%s"
+import json
+
+db = sqlite3.connect("profile_results.sqlite")
 
 times = ["%02d:%02d" % (h, m) for h in range(24) for m in range(0, 60, 30)]
 modes = ["WALK", "BICYCLE", "WALK,TRANSIT", "BICYCLE,TRANSIT"]
@@ -12,9 +16,67 @@ walks = [500, 1000, 2000, 3000, 5000, 10000, 20000]
 #runid = time;
 #db.execute("insert % into runs");
 
-for time in times:
-    for arrive_by in ("TRUE", "FALSE") :
-        for mode in modes :
-            for walk in walks :
-                print URL % (0,0,0,0,"QUICK",walk,mode,time,arrive_by)
+endpoints = {}
+for line in open('endpoints'):
+    fields = line.strip().split(',')
+    if len(fields) != 3:
+        print 'line does not have 3 fields'
+    else:
+        name, lat, lon = fields
+        endpoints[name] = (float(lat),float(lon))
+print endpoints
+
+URL = "http://localhost:8080/opentripplanner-api-webapp/ws/plan?submit&fromPlace=%f,%f&toPlace=%f,%f&min=%s&maxWalkDistance=%d&mode=%s&submit&time=%s&date=09/14/2011&arr=%s"
+
+total = 0
+worst = 0
+worst_url = None
+durations = []
+x = list(itertools.product(modes, times, walks, ('Depart','Arrive'), endpoints.items(), endpoints.items()))
+random.shuffle(x)
+for r in x :
+#for r in itertools.product(modes, times, walks, ('Depart','Arrive'), endpoints.items(), endpoints.items()) :
+    print r
+    (mode, t, walk, arrive_by, orig, dest) = r
+    o_name, o_coord = orig
+    d_name, d_coord = dest
+    if o_name == d_name :
+        continue
+    print '%s to %s' % (o_name, d_name)
+    url = URL % (o_coord[0],o_coord[1],d_coord[0],d_coord[1],"QUICK",walk,mode,t,arrive_by)
+    req = urllib2.Request(url)
+    req.add_header('Accept', 'APPLICATION/XML')
+    start = time.time()
+    response = urllib2.urlopen(req).read()
+    end = time.time()
+    #print response
+    #response = json.loads(response)
+    #print json.dumps(response, sort_keys=False, indent=4)
+    soup = BeautifulStoneSoup(response)
+    #print soup.prettify()
+    #if 'error' in response :
+    #    print 'error'
+    #    continue
+    #nItin = len(response['plan']['itineraries']['itinerary'])
+    if (soup.response.error != None) :
+        print 'error'
+        continue
+    nItin = len(soup.response.plan.itineraries)
+    print nItin, 'itineraries'
+    duration = end - start
+    total += duration
+    print duration, 'sec'
+    print url
+    durations.append(duration)
+    if duration > worst:
+        worst = duration
+        worst_url = url
+
+durations.sort()
+#print durations
+print "median", durations[(len(durations)-1)/2]
+print "average", (total / len(urls))
+print "worst", worst
+#print "worst url", worst_url
+
 
